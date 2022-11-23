cbc416946606e865ec83c98fb5717e5458b4ba2c
bengalbits <116909013+bengalbits@users.noreply.github.com>
2022-11-23 23:13:20 +0530

    added comment4

--- Smart_Umbrella.ino
+++ Smart_Umbrella.ino
@@ -42,6 +42,6 @@ void loop()
     else if(Incoming_value == '3')       
       digitalWrite(7, HIGH);
     else if(Incoming_value == '4')       
-      digitalWrite(7, LOW); // comment3
+      digitalWrite(7, LOW); // comment4
   }
 } 
